f168c76a22d71ee468276e5019fedcdf04d84b3e
unknown <>
2016-10-09 15:40:54 +0900

    delete duplicated login directory

--- finalproject.py
+++ finalproject.py
@@ -35,15 +35,6 @@ def showLogin():
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
 
-# Create anti-forgery state token
-@app.route('/login')
-def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in xrange(32))
-    login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
-    return render_template('login.html', STATE=state)
-
 
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
